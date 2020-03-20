e827d00172df2d39dead3c6ffa3be9332883b145
Kevin Tang <kevintang300@gmail.com>
2020-03-19 22:07:41 -0700

    Initial Commit

---
+++ Tippie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippie
+//
+//  Created by Kevin Tang on 3/19/20.
+//  Copyright © 2020 Kevin Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
